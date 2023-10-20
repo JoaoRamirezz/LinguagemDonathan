5ae3f81905efa95c048272a3f3148fb1901a9bd7
Andrey <akdagmr007s@gmail.com>
2023-10-20 13:58:04 -0300

    segundo commit - melhoria do profetizou para identificar somas

--- compiler.py
+++ compiler.py
@@ -3,43 +3,75 @@ ascii = []
 
 file = "index.don"
 
+
+
+def asciiConvert(variable):
+    w = [ord(c) for c in variable]
+    
+    for i in w:
+        ascii.append(i)
+        
+    ascii.append(0)
+    
+            
+            
+            
 try:
     with open(file, 'r') as f:
         lines = f.readlines()
         for i in lines:
             string = i
             if "profetizou(" in i:
+                
                 aspas_1 = 0
                 aspas_2 = 0
                 count_aspas = 0
+                
                 try:
                     for j in i:
                         if j != "\"" and count_aspas == 0:
                             aspas_1 += 1
                             aspas_2 += 1
-                            
-                            
+                        
                         elif j == "\"":
                             count_aspas += 1
                             
                         elif j != "\"" and count_aspas == 1:
                             aspas_2 += 1
+                        
+                    
+                    if count_aspas == 0:
+                        a = 0
+                        first = True
+                        operation = ""
+                        
+                        for j in i:
+                            if j.isdigit() and first:
+                                a += int(j)
+                                first = False
+                                
+                            if j == "+":
+                                operation = "sum"
+                                
+                            if j.isdigit() and operation == "sum":
+                                a += int(j)
                             
-                    if (count_aspas < 2):
+                        asciiConvert(str(a))
+                                
+                            
+                    elif(count_aspas == 1):
                         print("n fechou as aspas")
                     
                     else:
                         string = string[aspas_1+1:aspas_2+1]
-                        print(string)
+                        asciiConvert(string)
+                    
                 except:
                     print("Don não sabe até onde profetizar")
+        
+        print(ascii)
                
-            # w = [ord(c) for c in string]
-            # for i in w:
-            #     ascii.append(i)
-                
-            # ascii.append(0)
-            # print(ascii)
+
                 
 except:
     print("don esta desaparecido")           
